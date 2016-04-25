b62f6da23cd10c47846d5a72378fbfe5aeeb8ca8
Samantha Hoy <samantha.hoy@gmail.com>
2016-04-25 17:55:23 +0200

    Add function for totalEarningsForEachTaxi

--- totEarningsForEachTaxi.js
+++ totEarningsForEachTaxi.js
@@ -1,19 +1,18 @@
-
 // What are the total earnings for each taxi?
 
 module.exports = function(Taxis) {
-var vehicleDetails = {};
+  var vehicleDetails = {};
 
-Taxis.forEach(function(taxis){
-  var carReg = taxis.RegistrationNumber;
-  var totalEarnings = Number(taxis.Trips * taxis.Fare) ;
+  Taxis.forEach(function(taxis) {
+    var carReg = taxis.RegistrationNumber;
+    var totalEarnings = Number(taxis.Trips * taxis.Fare);
 
-  if(vehicleDetails[carReg] === undefined) {  // if value doesnt exist in the new array
-    vehicleDetails[carReg] = 0 ;              // initialise vehicleDetails.carReg to 0 - will print the carReg : 0
+    if (vehicleDetails[carReg] === undefined) { // if value doesnt exist in the new array
+      vehicleDetails[carReg] = 0; // initialise vehicleDetails.carReg to 0 - will print the carReg : 0
     }
 
-  vehicleDetails[carReg] = vehicleDetails[carReg] + totalEarnings ; // add totalEarnings to previous total
-})
+    vehicleDetails[carReg] = vehicleDetails[carReg] + totalEarnings; // add totalEarnings to previous total
+  })
   console.log(vehicleDetails);
   return vehicleDetails;
 }
